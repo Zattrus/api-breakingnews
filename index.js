4eee8c17f866ac85de92fe2dfc981c782f29b699
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2024-01-15 14:29:43 -0300

    refactor: config route create user and controller

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const userRoute = require('./src/routes/user.routes')
 
+const port = "http://localhost:3000";
+
 const app = express();
+app.use(express.json());
+app.use('/user', userRoute)
 
-app.use('/soma', userRoute)
-
-app.listen(3000)
+app.listen(3000, () => console.log(`Servidor rodando na porta ${port}`))
